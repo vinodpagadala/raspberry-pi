eea176c4ee20c73efa26a513af7eb83cd501f8d1
Vinod Pagadala <vinodpagadala241@gmail.com>
2017-03-02 20:10:51 -0500

    Create ledBlinker.py
    Python code to lit up LED bulbs to create a certain pattern.

---
+++ ledBlinker.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+GPIO.setup(17,GPIO.OUT)
+GPIO.setup(25,GPIO.OUT)
+while True:
+	GPIO.output(17,GPIO.HIGH)
+	time.sleep(0.5)
+	GPIO.output(25,GPIO.HIGH)
+	time.sleep(1)
+	GPIO.output(17,GPIO.LOW)
+	time.sleep(0.5)
+	GPIO.output(25,GPIO.LOW)
+	time.sleep(1)
